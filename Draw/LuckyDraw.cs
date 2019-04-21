8bd29ae6808abb81218f1e3d914875d2b7801b21
xzh1998 <46914173+xzh1998@users.noreply.github.com>
2019-04-22 00:30:15 +0800

    初期版本

---
+++ Draw/LuckyDraw.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draw
+{
+    class LuckyDraw
+    {
+        public static int ll;
+        public static int[] personSpace;
+        public List<GroupMember> CreatLuckyDraw(List<GroupMember> GroupList , Condition condition)
+        {
+            List<GroupMember> WinnerGroupList = new List<GroupMember>();
+            createPearsonSpace(GroupList);
+            int WinnerNum = condition.firstPrizeNumber + condition.secondPrizeNumber + condition.thirdPrizeNumber;
+            Boolean isPrise = false;
+            if (WinnerNum > GroupList.Count)
+            {
+                return GroupList;
+            }
+            for(int i = 0; i < WinnerNum;)
+            {
+                int ran = RandomNum(GroupList.Count);
+                for(int j = 0; j < personSpace.Length; j++)
+                {
+                    if (ran <= personSpace[j] && WinnerGroupList.Count == 0)
+                    {
+                        GroupMember temp = new GroupMember();
+                        temp = GroupList[j];
+                        WinnerGroupList.Add(temp);
+                        i++;
+                        break;
+                    }
+                    else if (ran <= personSpace[j] && WinnerGroupList.Count != 0)
+                    {
+                        for(int k = 0; k < WinnerGroupList.Count; k++)
+                        {
+                            if (GroupList[j].name.Equals(WinnerGroupList[k].name))
+                            {
+                                isPrise = true;
+                            }
+                        }
+                        if (!isPrise)
+                        {
+                            GroupMember temp = new GroupMember();
+                            temp = GroupList[j];
+                            WinnerGroupList.Add(temp);
+                            i++;
+                            break;
+                        }
+                        else
+                        {
+                            isPrise = false;
+                            break;
+                        }
+                    }
+                }
+            }
+            return WinnerGroupList;
+        }
+
+        public static void createPearsonSpace(List<GroupMember> GroupList)
+        {
+            personSpace = new int[GroupList.Count];
+            personSpace[0] = 0;
+            for (int i = 0; i < GroupList.Count; i++)
+            {
+                if (i == 0)
+                {
+                    personSpace[i] = GroupList[i].Stimes;
+                }
+                else
+                {
+                    personSpace[i] = GroupList[i].Stimes + personSpace[i - 1];
+                }
+            }
+        }
+
+        public static int RandomNum(int n)
+        {
+            int max = 10;
+            RNGCryptoServiceProvider csp = new RNGCryptoServiceProvider();
+            byte[] byteCsp = new byte[10];
+            csp.GetBytes(byteCsp);
+            int t = Convert.ToInt32(byteCsp[0]) % max;
+            int m = personSpace[n - 1];
+            int a = 9;
+            int b = 7;
+            ll = personSpace[n - 1];
+            Random random = new Random();
+            int ran = random.Next(0, ll);
+            for (int i = 1; i < t; i++)
+            {
+                ran = (a * ran + b) % m;
+            }
+            return ran;
+        }
+    }
+}
